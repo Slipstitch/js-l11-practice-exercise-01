7c038eeb95dd7a0938e2158a371859249e43456d
Sheila Harder <56269957+Slipstitch@users.noreply.github.com>
2021-05-18 15:29:41 -0500

    Update index.js
    fetch random profile data from random profile generator

--- js/index.js
+++ js/index.js
@@ -1,1 +1,30 @@
 const randomFolks = document.querySelector(".random-peeps");
+
+const getData = async function () {
+	const usersRequest = await fetch (
+	"https://randomuser.me/api?results=5");
+	const data = await usersRequest.json();
+    //console.log(usersRequest);
+    const userResults = data.results; //array of objects
+    //console.log(userResults); //make sure grabbed right data
+    displayUsers(userResults);
+};
+getData();
+
+const displayUsers = function (userResults) {
+	randomFolks.innerHTML = "";
+	//empty element contents to avoid duplicate DOM elements
+	//loop over userResults
+	for (const user of userResults) {
+		const country = user.location.country;
+		const name = user.name.first;
+		const imageUrl = user.picture.medium;
+		const userDiv = document.createElement("div");
+		userDiv.innerHTML = `
+		   <h3>${name}</h3>
+		   <p>${country}</p>
+		   <img src=${imageUrl} alt="User Avatar"/>
+		   `; 
+		randomFolks.append(userDiv);  
+	}
+};
